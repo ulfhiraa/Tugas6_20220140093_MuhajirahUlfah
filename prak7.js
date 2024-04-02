ef7b264f1f6f257ce54c04d69297b239dbd4326f
ulfhiraa <studyiraa@gmail.com>
2024-04-02 20:20:28 +0700

    first commit

---
+++ prak7.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("It's Not Living (If It's Not With You) performed by the English rock band The 1975 live at Reading + Leeds 2019. ");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("This is the Original audio of It's Not Living (If It's Not With You). ");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("This song was formally released on October 18, 2018.");
+});
